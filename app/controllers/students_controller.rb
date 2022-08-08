fa3da742f811fd6c97978ea069e17d24c6ddee94
Willie Shi <wshi9124@gmail.com>
2022-08-08 15:20:59 -0400

    commit tree 25bfba3c51cdfd3cdc45586d56e3c481e6bbc336

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,9 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def students
+    id= params[:id]
+    student= Student.find(id)
+    render json: student
+  end
 end
